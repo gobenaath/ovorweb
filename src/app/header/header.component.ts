5449632bc88cf81ba6aade2e588d713cad908181
gobenaath <gobenaath@gmail.com>
2021-07-01 18:33:21 -0500

    updated for donation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-header',
@@ -8,12 +9,12 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   urlString: string;
-
-  constructor(private router: Router) {}
+  modalRef: BsModalRef;
+  constructor(private router: Router, private modalService: BsModalService) {}
 
   ngOnInit(): void {
-    console.log(this.router.url);
     this.urlString = this.router.url;
+    
   }
 
   getUrl() {
@@ -34,5 +35,48 @@ export class HeaderComponent implements OnInit {
       return '/news';
     }
     return this.urlString;
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, { class: 'modal-md' });
+  }
+
+  close(): void {
+    this.modalRef.hide();
+  }
+
+  
+
+  DonationData = {
+    title: 'For Donation...',
+    description:
+      "All donations will be spent on various social activities like developing Government School infrastructure, village infrastructure, need during calamities, etc. " + 
+      "<br/><br/><p><b><i>We accept donation only from INDIAN ACCOUNTS</i></b></p>",
+    items: [
+      {
+        title: 'Acc No',
+        value: '335601010034211',  
+      },
+      {
+        title: 'IFSC Code',
+        value: 'UBIN0533564',  
+      },
+      {
+        title: 'Branch',
+        value: 'Watrap',  
+      },
+      {
+        title: 'Customer Id',
+        value: '900226910',  
+      },
+      {
+        title: 'Bank',
+        value: 'Union Bank Of India',  
+      },
+      {
+        title: 'Registered Trust Id (NGO No)',
+        value: '8/BK.4/2020',  
+      },
+    ]
   }
 }
